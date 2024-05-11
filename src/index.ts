12a44ebc20b06c9164c83761a46ad63ccafdd36b
Sanniv <sanniv.1020@gmail.com>
2024-05-12 02:57:39 +0530

    changes

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@ import { WebSocketServer } from 'ws'
 import fs from 'fs/promises';
 import path from 'path';
 import * as pty from 'node-pty'
+import { exec } from 'child_process'
 
 interface FileTreeNode {
-    id: number;
     name: string;
     type: 'directory' | 'file';
     filePath: string; // Add filePath property
@@ -21,9 +21,9 @@ app.use(express.json())
 
 var ptyProcess = pty.spawn('bash', [], {
   name: 'xterm-color',
-  cols: 80,
+  cols: 120,
   rows: 30,
-  cwd: process.env.INIT_CWD,
+  cwd: '../user',
   env: process.env
 });
 
@@ -34,11 +34,21 @@ const httpServer = app.listen(PORT, () => {
 const wss = new WebSocketServer({server: httpServer})
 
 wss.on('connection', ws => {
-    let userCommand = ""
+    let userCommand = 'cd ../ && cd user' + '\r'
+
+    //init commands for dev purposes
+    exec('cd ../ && mkdir user && cd user && touch index.js && npm init -y')
+    ptyProcess.write('cd ../ && cd user' + '\r')
+    
     ws.on('message', data => {
         try {
-            ptyProcess.write(data.toString() + "\r");
-            userCommand = data.toString()
+            if(data.toString().includes('sudo') || data.toString().includes('rm -r user') || data.toString().includes('rm index.js') || data.toString().includes('rm package.json')){
+                ptyProcess.write("\r");
+            }
+            else{
+                ptyProcess.write(data.toString() + "\r");
+                userCommand = data.toString()
+            }
         } catch (error) {
             ws.send(`Error: ${error}`);
         }
@@ -54,7 +64,7 @@ wss.on('connection', ws => {
 
 app.get('/files', async (_, res) => {
     try {
-        const fileTree = await generateFileTree("./")
+        const fileTree = await generateFileTree("../user")
         res.json({ tree: fileTree })
     } catch (error) {
         res.status(500).json({ error: "Internal server error" })
@@ -71,12 +81,34 @@ app.get('/file', async (req, res) => {
     try{
         await fs.access(filePath as string, fs.constants.R_OK | fs.constants.W_OK)
         const contents = await fs.readFile(filePath as string, { encoding: 'utf8' });
-        res.status(200).json({contents})
+        res.status(200).json({code: contents, filePath})
     }
     catch(e){
         res.status(400).send("File Not Found")
     }
 });
+
+app.get('/run', (_, res) => {
+    try{
+        ptyProcess.write('node index.js' + '\r')
+        res.status(200).send("Executed Succesfully")
+    }
+    catch(e){
+        res.send(400).send("Unexpected error occured")
+    }
+})
+
+app.put('/code', async (req, res) => {
+    try{
+        const { code, filePath } = req.body.data
+        await fs.access(filePath as string, fs.constants.R_OK | fs.constants.W_OK)
+        await fs.writeFile(filePath as string, code)
+        res.send(200)
+    }
+    catch(e){
+        res.status(400).send("Unexpected error occured")
+    }
+})
 
 async function generateFileTree(dirPath: string): Promise<FileTreeNode[]> {
     const files: string[] = await fs.readdir(dirPath);
@@ -87,7 +119,6 @@ async function generateFileTree(dirPath: string): Promise<FileTreeNode[]> {
         const stats = await fs.stat(filePath);
 
         const node: FileTreeNode = {
-            id: 1,
             name: file,
             type: stats.isDirectory() ? 'directory' : 'file',
             filePath: filePath, // Set filePath property
